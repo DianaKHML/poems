681143192fb40dee8ac3f95b3f646f584e709879
DianaKHML <hmylnina_diana@mail.ru>
2024-03-21 17:04:56 +0500

    menu
    менюшки и текстовая страница

--- index.js
+++ index.js
@@ -2,7 +2,26 @@ import dataseif from "./js/data.js";
 
 let menu2 = document.getElementById('menu2')
 let divright = document.getElementById('divright')
+let stihcontent = document.getElementById('stihcontent')
 
+
+//добавление данных на страницу со стихом
+function stihtext (){
+    for (let i = 0; i<dataseif.stihzaglav.length; i++){
+        let v = document.createElement('h2')
+        v.textContent = dataseif.stihzaglav[i]
+        stihcontent.appendChild(v)
+        let p = document.createElement('p')
+        p.textContent = dataseif.stihinfo[stihcontent.className][i]
+        let hr = document.createElement('hr')
+        stihcontent.appendChild(p)
+        stihcontent.appendChild(hr)
+    }
+}
+
+
+
+// Меню для главной страницы
 function menu (){
     let inp = document.createElement('input')
     inp.type = 'checkbox'
@@ -37,4 +56,52 @@ function menu (){
         a.href = dataseif.menu[i][z]
     }
 }
-menu()
+// Меню для остальных страниц
+function menupage (){
+    let inp = document.createElement('input')
+    inp.type = 'checkbox'
+    inp.id = 'active'
+
+    let label = document.createElement('label')
+    label.htmlFor = 'active'
+    label.className = 'menu-btn'
+    label.textContent = 'Menu'
+
+    let menudiv = document.createElement('div')
+    menudiv.className = 'wrapper'
+
+    let menuul = document.createElement('ul')
+    menuul.id = 'menu'
+    menudiv.appendChild(menuul)
+
+    divright.appendChild(inp)
+    divright.appendChild(label)
+    divright.appendChild(menudiv)
+
+    let menu = document.getElementById('menu')
+
+    for (let i=0; i<dataseif.menu2.length; i++){
+        let z = 0
+        let li = document.createElement('li')
+        let a = document.createElement('a')
+        menu.appendChild(li)
+        li.appendChild(a)
+        a.textContent = dataseif.menu2[i][z]
+        z++
+        a.href = dataseif.menu2[i][z]
+    }
+}
+// Выбор меню в зависимости от открытой страницы
+if (document.title == 'Главная'){
+    menu()
+}
+else if (document.title == 'Автор'){
+    menupage()
+}
+else {
+    menupage()
+    stihtext()
+}
+
+
+
--- js/data.js
+++ js/data.js
@@ -8,7 +8,41 @@ let dataseif = {
         ['Сентябрь',`./page/Sentyabr.html`],
         ['Что греет душу?',`./page/Chto.html`],
         ['Об авторе',`./page/autor.html`]
+    ],
+    menu2: [
+        ['Главная', `../index.html`],
+        ['Вдохновение', `./Vdohnovenie.html`],
+        ['Осень', `./Osen.html`],
+        ['Счастье',`./Schaste.html`],
+        ['Как жаль, что...',`./Kak.html`],
+        ['Сентябрь',`./Sentyabr.html`],
+        ['Что греет душу?',`./Chto.html`],
+        ['Об авторе',`./autor.html`]
+    ],
+    stihzaglav: [
+        'Выбор названия', 
+        'Что вдохновило на создание', 
+        'Вложеный смысл', 
+        'При каких обстоятельствах было написано', 
+        'Где участвовало, размещалось'
+    ],
+    stihinfo: [
+        [
+            '1 klbkbb123123123123jortibjortb',
+            '2 kjbbviibbucvquyv',
+            '3 uiqhfkl123211213213njlkgrtuib',
+            '4 kljfvjo;iehnbhtiub',
+            '5 iehguiwghiuewrg'
+        ],
+        [
+            '6 klbkbb123123123123jortibjortb',
+            '7 kjbbviibbucvquyv',
+            '8 uiqhfkl123211213213njlkgrtuib',
+            '9 kljfvjo;iehnbhtiub',
+            '10 iehguiwghiuewrg'
+        ]
     ]
+
 }
 
 export default dataseif
