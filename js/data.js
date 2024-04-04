d6418c02c007ed9a450b1cdf341aafca4719917d
DianaKHML <hmylnina_diana@mail.ru>
2024-04-04 18:56:31 +0500

    123
    123

--- js/data.js
+++ js/data.js
@@ -1,47 +1,132 @@
 let dataseif = {
-    //меню для главной страницы
-    menu: [
-        ['Главная', `./index.html`],
-        ['Вдохновение', `./page/Vdohnovenie.html`],
-        ['Осень', `./page/Osen.html`],
-        ['Счастье',`./page/Schaste.html`],
-        ['Как жаль, что...',`./page/Kak.html`],
-        ['Сентябрь',`./page/Sentyabr.html`],
-        ['Что греет душу?',`./page/Chto.html`],
-        ['Об авторе',`./page/autor.html`]
-    ],
-    //меню для других страниц
-    menu2: [
-        ['Главная', `../index.html`],
-        ['Вдохновение', `./Vdohnovenie.html`],
-        ['Осень', `./Osen.html`],
-        ['Счастье',`./Schaste.html`],
-        ['Как жаль, что...',`./Kak.html`],
-        ['Сентябрь',`./Sentyabr.html`],
-        ['Что греет душу?',`./Chto.html`],
-        ['Об авторе',`./autor.html`]
-    ],
-    //заголовки на стр со стихом
-    stihzaglav: [
-        'Выбор названия', 
-        'Что вдохновило на создание', 
-        'Вложеный смысл', 
-        'Где участвовало, размещалось'
-    ],
-    //параграфы на стр со стихами
-    stihinfo: [
-        [
-            '1 klbkbb123123123123jortibjortb',
-            '2 kjbbviibbucvquyv',
-            '3 uiqhfkl123211213213njlkgrtuib',
-            '4 kljfvjo;iehnbhtiub'
-        ],
-        [
-            '5 klbkbb123123123123jortibjortb',
-            '6 kjbbviibbucvquyv',
-            '7 uiqhfkl123211213213njlkgrtuib',
-            '8 kljfvjo;iehnbhtiub'
-        ]
+    
+    // Меню майта
+    menu3:{
+        Glavnaya:{
+            menutext:'Главная',
+            menuurl:'index.html',
+            class:'0'
+        },
+        autor:{
+            menutext:'Автор',
+            menuurl:'autor.html',
+            class:'1'
+        },
+        Vdohnovenie:{
+            menutext:'Вдохновение',
+            menuurl:'Vdohnovenie.html',
+            class:'2'
+        },
+        Osen:{
+            menutext:'Осень',
+            menuurl:'Osen.html',
+            class:'3'
+        },
+        Schaste:{
+            menutext:'Счастье',
+            menuurl:'Schaste.html',
+            class:'4'
+        },
+        Kak:{
+            menutext:'Как жаль, что...',
+            menuurl:'Kak.html',
+            class:'5'
+        },
+        Sentyabr:{
+            menutext:'Сентябрь',
+            menuurl:'Sentyabr.html',
+            class:'6'
+        },
+        Chto:{
+            menutext:'Что греет душу?',
+            menuurl:'Chto.html',
+            class:'7'
+        },
+        Solnce:{
+            menutext:'Солнце',
+            menuurl:'Solnce.html',
+            class:'8'
+        }
+    },
+
+    // Данные для страниц со стихом
+    testmass: {
+        Vdohnovenie: {
+            zagolovok:'Вдохновение',
+            vibornazvaniya:'Выбор названия',
+            chtovdoh:'Что вдохновило',
+            vlogsmysl:'Вложенный смысл',
+            gdeychastie:'Где участие',
+            img:'./images/Vdohnovenie.jpg'
+        },
+        Osen: {
+            zagolovok:'Осень',
+            vibornazvaniya:'1 2',
+            chtovdoh:'2 2',
+            vlogsmysl:'3 2',
+            gdeychastie:'4 2',
+            img:'./images/Osen.jpg'
+        },    
+        Schaste: {
+            zagolovok:'Счастье',
+            vibornazvaniya:'1 2',
+            chtovdoh:'2 2',
+            vlogsmysl:'3 2',
+            gdeychastie:'4 2',
+            img:'./images/Schaste.jpg'    
+        },
+        Kak: {
+            zagolovok:`Как жаль, что...`,
+            vibornazvaniya:'1 2',
+            chtovdoh:'2 2',
+            vlogsmysl:'3 2',
+            gdeychastie:'4 2',
+            img:'./images/Kak.jpg'    
+        },
+        Sentyabr: {
+            zagolovok:'Сентябрь',
+            vibornazvaniya:'1 2',
+            chtovdoh:'2 2',
+            vlogsmysl:'3 2',
+            gdeychastie:'4 2',
+            img:'./images/Sentuabr.jpg'    
+        },
+        Chto: {
+            zagolovok:'Что греет душу?',
+            vibornazvaniya:'1 2',
+            chtovdoh:'2 2',
+            vlogsmysl:'3 2',
+            gdeychastie:'4 2',
+            img:'./images/Chto.jpg'    
+        },
+        Solnce: {
+            zagolovok:'Солнце',
+            vibornazvaniya:'1 2',
+            chtovdoh:'2 2',
+            vlogsmysl:'3 2',
+            gdeychastie:'4 2',
+            img:'./images/Solnce.jpg'    
+        }   
+    },
+
+    //Заголовки и классы элементов на странице со стихом
+    stihzaglav1:[
+        {
+            zag: 'Выбор названия',
+            class: 'vibornazvaniya'
+        },
+        {
+            zag: 'Что вдохновило на создание',
+            class: 'chtovdoh'
+        },
+        {
+            zag: 'Вложеный смысл',
+            class: 'vlogsmysl'
+        },
+        {
+            zag: 'Где участвовало, размещалось',
+            class: 'gdeychastie'
+        }
     ],
     //стр автора
     autorinfo:[
@@ -53,10 +138,7 @@ let dataseif = {
             h2: 'Творчество',
             p: 'Начало творчества'
         }
-
     ]
-
 }
 
 export default dataseif
-
