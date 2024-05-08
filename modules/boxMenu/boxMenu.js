4854d99e085a839053c6612e2cbadb1f76776d6d
DianaKHML <hmylnina_diana@mail.ru>
2024-05-08 20:40:30 +0500

    and work and
    теперь ссылки работают при нажатии на сам блок с ссылкой, а не только на надпись

--- modules/boxMenu/boxMenu.js
+++ modules/boxMenu/boxMenu.js
@@ -9,32 +9,26 @@ rightbox.appendChild(divBoxMenu)
 
 if (document.title == 'Glavnaya' || document.title == 'autor' ){
     let nfgnjgjkdfgkj = `
-        <div class="boxMenu">
-        <a href="https://www.wattpad.com/story/353406811?utm_source=android&utm_medium=org.telegram.messenger&utm_content=share_reading&wp_page=reading&wp_uname=Lim_Neyson">сборник стихов</a>
-        </div>
-        <div class="boxMenu">
-        <a href="https://t.me/Lima_Ogonyok">телеграм-канал</a>
-        </div>
-        <div class="boxMenu">
-        <a href="https://forms.yandex.ru/u/657c9449c417f317a89d34cb/">задать вопрос автору</a>
-        </div>
+        <a href="https://www.wattpad.com/story/353406811?utm_source=android&utm_medium=org.telegram.messenger&utm_content=share_reading&wp_page=reading&wp_uname=Lim_Neyson">
+        <div class="boxMenu">сборник стихов</div></a>
+        <a href="https://t.me/Lima_Ogonyok">
+        <div class="boxMenu">телеграм-канал</div></a>
+        <a href="https://forms.yandex.ru/u/657c9449c417f317a89d34cb/">
+        <div class="boxMenu">задать вопрос автору</div></a>
     `
     divBoxMenu.innerHTML = nfgnjgjkdfgkj
 } 
 else {
      let nfgnjgjkdfgkj = `
-        <div class="boxMenu">
-        <a href="${dataseif.testmass[document.title].link}">читать стихотворение</a>
-        </div>
-        <div class="boxMenu">
-        <a href="https://www.wattpad.com/story/353406811?utm_source=android&utm_medium=org.telegram.messenger&utm_content=share_reading&wp_page=reading&wp_uname=Lim_Neyson">сборник стихов</a>
-        </div>
-        <div class="boxMenu">
-        <a href="https://t.me/Lima_Ogonyok">телеграм-канал</a>
-        </div>
-        <div class="boxMenu">
-        <a href="https://forms.yandex.ru/u/657c9449c417f317a89d34cb/">задать вопрос автору</a>
-        </div>
+        <a href="${dataseif.testmass[document.title].link}">
+        <div class="boxMenu">читать стихотворение</div>
+        <a href="https://www.wattpad.com/story/353406811?utm_source=android&utm_medium=org.telegram.messenger&utm_content=share_reading&wp_page=reading&wp_uname=Lim_Neyson">
+        <div class="boxMenu">сборник стихов</div></a>
+        <a href="https://t.me/Lima_Ogonyok">
+        <div class="boxMenu">телеграм-канал</div></a>
+        <a href="https://forms.yandex.ru/u/657c9449c417f317a89d34cb/">
+        <div class="boxMenu">задать вопрос автору</div></a>
+        
     `
     divBoxMenu.innerHTML = nfgnjgjkdfgkj
 }
