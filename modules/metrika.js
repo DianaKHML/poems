a9659074b84ffe94af4f7cd7a62f97bca4d15964
DianaKHML <hmylnina_diana@mail.ru>
2024-05-14 15:46:54 +0500

    scetmail
    счётчик майл ру

--- modules/metrika.js
+++ modules/metrika.js
@@ -30,6 +30,20 @@ let fignya = `
 
   gtag('config', 'G-C915VT5L27');
 </script>
+<!-- Top.Mail.Ru counter -->
+<script type="text/javascript">
+var _tmr = window._tmr || (window._tmr = []);
+_tmr.push({id: "3513717", type: "pageView", start: (new Date()).getTime()});
+(function (d, w, id) {
+  if (d.getElementById(id)) return;
+  var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+  ts.src = "https://top-fwz1.mail.ru/js/code.js";
+  var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+  if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+})(document, window, "tmr-code");
+</script>
+<noscript><div><img src="https://top-fwz1.mail.ru/counter?id=3513717;js=na" style="position:absolute;left:-9999px;" alt="Top.Mail.Ru" /></div></noscript>
+<!-- /Top.Mail.Ru counter -->
 `
 
 divBoxMenu.innerHTML = fignya
